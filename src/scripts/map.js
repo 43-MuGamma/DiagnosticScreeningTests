6332bfb43b6b9aadef9dab24aa046a818a3e5aa4
Jimmy Klein <jimmy.klein4@gmail.com>
2016-04-23 21:42:07 -0400

    Basic places working

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,4 +1,19 @@
-
+function getParameterByName(name, url) {
+  "use strict";
+  if (!url) {
+    url = window.location.href;
+  }
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+    results = regex.exec(url);
+  if (!results) {
+    return null;
+  }
+  if (!results[2]) {
+    return '';
+  }
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 function initMap() {
   if(navigator.getlocation){
@@ -20,17 +35,27 @@ function initMap() {
   var request = {
     location: philadelphia,
     radius: '500',
-    types: ['store']
+    types: ['physiotherapist']
   };
 
   var service = new google.maps.places.PlacesService(map);
   service.nearbySearch(request, function(results, status) {
     if(status == google.maps.places.PlacesServiceStatus.OK) {
+      var infowindow = new google.maps.InfoWindow({
+        content: "holding..."
+      });
       for (var i=0; i < results.length; i++) {
         var place = results[i];
+        console.log(place);
+        var name = place.name + "\n\n" + place.vicinity;
         var marker = new google.maps.Marker({
           map: map,
+          title:name,
           position: place.geometry.location
+        });
+        marker.addListener('click', function(){
+          infowindow.setContent(this.title);
+          infowindow.open(map, this);
         });
       }
     }
